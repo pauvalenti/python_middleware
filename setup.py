a94de92f41ad55ac3ddd10dee2f608fc20d7815c
Pau Valentí <pauvalenti94@gmail.com>
2020-09-29 10:27:13 +0200

    _

--- setup.py
+++ setup.py
@@ -1,13 +1,13 @@
 from setuptools import setup
 
 setup(
-    name='python-middleware',
+    name='python_middleware',
     version='0.1.0',
     url='https://github.com/pauvalenti/python-middleware',
     author='Pau Valentí',
     author_email='pauv@example.es',
     license='',
-    packages=['python-middleware'],
+    packages=['python_middleware'],
     description='Flask Middleware',
     long_description='A python middleware for Flask APIs',
     keywords=['middleware', 'API'],
